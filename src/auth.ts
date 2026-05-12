6c7477178558d474b3781fdc3d6d10f61994b68a
Pavithra <161319747+Pavi2912@users.noreply.github.com>
2026-05-12 23:27:44 +0530

    Update auth.ts

--- src/auth.ts
+++ src/auth.ts
@@ -9,6 +9,10 @@ app.use(express.urlencoded({ extended: true }));
 app.post(
   '/login',
   passport.authenticate('local', { failureRedirect: '/login' }),
+  function (req, res) {
+    // Noncompliant - no session.regenerate after login
+    res.redirect('/');
+  }
 );
 
 export default app;
